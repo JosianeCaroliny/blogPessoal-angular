b6add18edeeb38aba09c524dc05d32be9dab35db
Josiane <jcarolliny@gmail.com>
2022-01-30 14:47:08 -0300

    [Task: Implementação do post e get de postagens]

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -23,6 +23,10 @@ export class AuthService {
     return this.http.post<User>('https://blogpessoaldajosi.herokuapp.com/usuarios/cadastrar', user)
   }
 
+  getByIdUser(id: number): Observable<User> {
+    return this.http.get<User>(`https://blogpessoaldajosi.herokuapp.com/usuarios/${id}`)
+  }
+
   logado() {
     let ok: boolean = false
 
